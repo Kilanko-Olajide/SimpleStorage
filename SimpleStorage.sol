aae825016278f84ea23e010bb0176dd9ef99c38f
Kilanko-Olajide <kilankoolajide@gmail.com>
2024-11-09 10:59:34 +0100

    Create SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,26 @@
+SPDX-LICENSE-IDENTIFIER: MIT
+pragma solidity ^0.6.0
+
+contract SimpleStorage{
+    uint256 favouriteNumber; 
+
+    struct People {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    People[] public people;
+    mapping(string => uint256) public nametofavoritenumber;
+
+    
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;    
+    }
+    function retrieve() public view returns (uint256) {
+        return favouriteNumber;
+    }
+    function addperson(string memory _name, uint256 _favouriteNumber) public {
+        people.push(People({favouriteNumber: _favouriteNumber, name: _name}))
+        nametofavoritenumber[_name] = _favouriteNumber;
+    }
+}
